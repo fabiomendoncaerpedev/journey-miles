f73c981f1e41e6a14b27b8a404623132ec9b0bdc
Fabio Mendonça <fabio.mendonca@totvs.com.br>
2024-09-24 16:54:39 -0300

    feat: creation of testimony and offers components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { BrowserModule } from '@angular/platform-browser';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
 import { HeaderComponent } from './shared/header/header.component';
 import { BannerComponent } from './shared/banner/banner.component';
@@ -11,6 +12,8 @@ import { ContainerComponent } from './shared/container/container.component';
 import { HomeComponent } from './pages/home/home.component';
 import { FooterComponent } from './shared/footer/footer.component';
 import { CardComponent } from './shared/card/card.component';
+import { CardBuscaComponent } from './shared/card-busca/card-busca.component';
+import { CardTestimonyComponent } from './shared/card-testimony/card-testimony.component';
 
 @NgModule({
   declarations: [
@@ -20,14 +23,17 @@ import { CardComponent } from './shared/card/card.component';
     ContainerComponent,
     HomeComponent,
     FooterComponent,
-    CardComponent
+    CardComponent,
+    CardBuscaComponent,
+    CardTestimonyComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     MatToolbarModule,
-    MatButtonModule
+    MatButtonModule,
+    MatCardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
