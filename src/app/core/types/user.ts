2ee9cfd5d62a8c888614e7b5d568de700f38bfc6
Fabio Mendonça <fabio.mendonca@totvs.com.br>
2024-09-30 06:40:15 -0300

    feat: user interface creation

---
+++ src/app/core/types/user.ts
@@ -0,0 +1,13 @@
+import { UF } from "./uf";
+
+export interface User {
+  nome: string,
+  nascimento: string,
+  cpf: string,
+  telefone: string,
+  email: string,
+  senha: string,
+  genero: string,
+  cidade: string,
+  estado: UF
+}
