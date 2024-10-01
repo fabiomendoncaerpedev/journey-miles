d7225579e3dbd9882b737d95aff339b1f08b626f
Fabio Mendonça <fabio.mendonca@totvs.com.br>
2024-10-01 15:20:26 -0300

    feat: implementation and applying of form validator

--- src/app/shared/form-base/form-base.component.ts
+++ src/app/shared/form-base/form-base.component.ts
@@ -2,6 +2,7 @@ import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { FormGroup, FormControl, Validators, FormBuilder } from '@angular/forms';
 import { UF } from 'src/app/core/types/types';
 import { FormService } from './../../core/services/form.service';
+import { FormValidations } from '../validators/form-validations';
 
 @Component({
   selector: 'app-form-base',
@@ -26,13 +27,13 @@ export class FormBaseComponent implements OnInit {
       bornDate: [null, [Validators.required]],
       cpf: [null, [Validators.required]],
       city: [null, Validators.required],
-      email: [null, [Validators.required, Validators.email]],
-      password: [null, [Validators.required, Validators.minLength(3)]],
       gender: ['outro'],
       phone: [null, Validators.required],
       state: this.stateControl,
-      confirmEmail: [null, [Validators.required, Validators.email]],
-      confirmPassword: [null, [Validators.required, Validators.minLength(3)]],
+      email: [null, [Validators.required, Validators.email]],
+      confirmEmail: [null, [Validators.required, Validators.email, FormValidations.equalTo('email')]],
+      password: [null, [Validators.required, Validators.minLength(3)]],
+      confirmPassword: [null, [Validators.required, Validators.minLength(3), FormValidations.equalTo('password')]],
       acceptTerms: [null, [Validators.requiredTrue]]
     });
 
