e6b380143e72a4972cb89f5c74dcc9b0bc12ce85
Fabio Mendonça <fabio.mendonca@totvs.com.br>
2024-10-02 13:53:15 -0300

    feat: token service creation

---
+++ src/app/core/services/token.service.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@angular/core';
+
+const KEY = 'token';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TokenService {
+
+  constructor() { }
+
+  saveToken(token: string) {
+    localStorage.setItem(KEY, token);
+  }
+
+  deleteToken() {
+    localStorage.removeItem(KEY);
+  }
+
+  getToken(): string {
+    return localStorage.getItem(KEY) ?? '';
+  }
+
+  hasToken(): boolean {
+    return !!this.getToken();
+  }
+}
