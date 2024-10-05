d994402bec3bb933d20b48141aa02d35f5b9124e
Fabio Mendonça <fabio.mendonca@totvs.com.br>
2024-10-05 17:51:06 -0300

    feat: implementation of ticket service

--- src/app/core/types/types.ts
+++ src/app/core/types/types.ts
@@ -33,3 +33,40 @@ export interface User {
 export interface AuthResponse {
   access_token: string
 }
+
+export interface Result {
+  paginaAtual: number
+  ultimaPagina: number
+  total: number
+  precoMin: number
+  precoMax: number
+  resultado: Array<Ticket>
+}
+
+export interface Ticket {
+  tipo: string;
+  precoIda: number;
+  precoVolta: number;
+  taxaEmbarque: number;
+  conexoes: number;
+  tempoVoo: number;
+  origem: UF;
+  destino: UF;
+  companhia: Airline;
+  dataIda: Date;
+  dataVolta: Date;
+  total: number;
+  orcamento: Array<Budget>;
+}
+
+export interface Airline {
+  id: string;
+  nome: string;
+}
+
+export interface Budget {
+  descricao: string;
+  preco: number;
+  taxaEmbarque: number;
+  total: number
+}
--- src/app/pages/search/search.component.ts
+++ src/app/pages/search/search.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Ticket } from 'src/app/core/types/types';
+import { TicketsService } from './../../core/services/tickets.service';
 
 @Component({
   selector: 'app-search',
   templateUrl: './search.component.html',
   styleUrls: ['./search.component.scss']
 })
-export class SearchComponent {
+export class SearchComponent implements OnInit{
+  tickets: Array<Ticket> = [];
 
+  constructor(
+    private ticketsService: TicketsService
+  ) {}
+
+  ngOnInit(): void {
+    const defaultSearch = {
+      data: new Date().toISOString,
+      pagina: 1,
+      porPagina: 25,
+      somenteId: false,
+      passageirosAdultos: 1,
+      tipo: 'Executiva'
+    };
+
+    this.ticketsService.getTickets(defaultSearch).subscribe({
+      next: (response) => {
+        this.tickets = response.resultado;
+      }
+    })
+  }
 }
