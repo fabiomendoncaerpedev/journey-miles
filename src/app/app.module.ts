20c005b009e5c703a85c8d6b0f177ea0a2196406
Fabio Mendonça <fabio.mendonca@totvs.com.br>
2024-10-12 22:16:02 -0300

    refactor: modulating project by its respective features

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,42 +1,14 @@
-import { ComplementaryFiltersComponent } from './shared/form-search/complementary-filters/complementary-filters.component';
 import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
+import { HTTP_INTERCEPTORS } from '@angular/common/http';
 import { BrowserModule } from '@angular/platform-browser';
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatCardModule } from '@angular/material/card';
-import { MatSliderModule } from '@angular/material/slider';
-import { MatButtonModule } from '@angular/material/button';
-import { MatButtonToggleModule } from '@angular/material/button-toggle';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import  {MatAutocompleteModule } from '@angular/material/autocomplete';
-import { MatIconModule } from '@angular/material/icon';
-import { MatChipsModule } from '@angular/material/chips';
-import { MatCheckboxModule } from '@angular/material/checkbox';
-import { MatInputModule } from '@angular/material/input';
-import { MatNativeDateModule } from '@angular/material/core';
-import { MatDialogModule } from '@angular/material/dialog';
-import { MatDividerModule } from '@angular/material/divider';
+import { MatAutocompleteModule } from '@angular/material/autocomplete';
 import { ReactiveFormsModule } from '@angular/forms';
-import { MatDatepickerModule } from '@angular/material/datepicker';
-import { MatRadioModule } from '@angular/material/radio';
-import { HeaderComponent } from './shared/header/header.component';
-import { BannerComponent } from './shared/banner/banner.component';
 import { AppRoutingModule } from './app-routing.module';
 import { SaleService } from './core/services/sale.service';
-import { ContainerComponent } from './shared/container/container.component';
-import { FooterComponent } from './shared/footer/footer.component';
-import { CardComponent } from './shared/card/card.component';
-import { CardSearchComponent } from './shared/card-search/card-search.component';
-import { ModalComponent } from './shared/modal/modal.component';
-import { CardTestimonyComponent } from './shared/card-testimony/card-testimony.component';
-import { FormSearchComponent } from './shared/form-search/form-search.component';
-import { CounterComponent } from './shared/counter/counter.component';
 import { HomeComponent } from './pages/home/home.component';
 import { AppComponent } from './app.component';
-import { DropdownUfComponent } from './shared/dropdown-uf/dropdown-uf.component';
 import { LoginComponent } from './pages/login/login.component';
-import { FormBaseComponent } from './shared/form-base/form-base.component';
 import { RegisterComponent } from './pages/register/register.component';
 import { PerfilComponent } from './pages/perfil/perfil.component';
 import { AuthenticationInterceptor } from './core/interceptors/authentication.interceptor';
@@ -46,64 +18,26 @@ import { FormService } from './core/services/form.service';
 import { TestimonyService } from './core/services/testimony.service';
 import { TokenService } from './core/services/token.service';
 import { UfService } from './core/services/uf.service';
-import { SearchComponent } from './pages/search/search.component';
 import { TicketsService } from './core/services/tickets.service';
-import { TicketComponent } from './shared/ticket/ticket.component';
-import { AirlinesComponent } from './shared/form-search/complementary-filters/airlines/airlines.component';
-import { LabelComponent } from './shared/form-search/complementary-filters/label/label.component';
-import { PricesComponent } from './shared/form-search/complementary-filters/prices/prices.component';
-import { StopsComponent } from './shared/form-search/complementary-filters/stops/stops.component';
 import { AirlineService } from './core/services/airline.service';
+import { SharedModule } from './shared/shared.module';
+import { MaterialModule } from './core/material/material.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent,
-    BannerComponent,
-    ContainerComponent,
-    HomeComponent,
-    FooterComponent,
-    CardComponent,
-    CardSearchComponent,
-    CardTestimonyComponent,
-    FormSearchComponent,
-    ModalComponent,
-    CounterComponent,
-    DropdownUfComponent,
-    LoginComponent,
-    FormBaseComponent,
-    RegisterComponent,
     PerfilComponent,
-    SearchComponent,
-    TicketComponent,
-    AirlinesComponent,
-    LabelComponent,
-    PricesComponent,
-    StopsComponent,
-    ComplementaryFiltersComponent
+    RegisterComponent,
+    LoginComponent,
+    HomeComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
+    SharedModule,
     BrowserAnimationsModule,
-    MatToolbarModule,
-    MatButtonModule,
-    MatCardModule,
-    MatButtonToggleModule,
-    MatIconModule,
-    MatChipsModule,
-    MatFormFieldModule,
-    MatInputModule,
-    MatDatepickerModule,
-    MatNativeDateModule,
-    MatDialogModule,
-    HttpClientModule,
-    ReactiveFormsModule,
-    MatAutocompleteModule,
-    MatRadioModule,
-    MatDividerModule,
-    MatCheckboxModule,
-    MatSliderModule
+    MaterialModule,
+    ReactiveFormsModule
   ],
   providers: [
     SaleService,
---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,74 @@
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { SearchComponent } from "../pages/search/search.component";
+import { BannerComponent } from "./banner/banner.component";
+import { CardSearchComponent } from "./card-search/card-search.component";
+import { CardTestimonyComponent } from "./card-testimony/card-testimony.component";
+import { CardComponent } from "./card/card.component";
+import { ContainerComponent } from "./container/container.component";
+import { CounterComponent } from "./counter/counter.component";
+import { DropdownUfComponent } from "./dropdown-uf/dropdown-uf.component";
+import { FooterComponent } from "./footer/footer.component";
+import { FormBaseComponent } from "./form-base/form-base.component";
+import { AirlinesComponent } from "./form-search/complementary-filters/airlines/airlines.component";
+import { ComplementaryFiltersComponent } from "./form-search/complementary-filters/complementary-filters.component";
+import { LabelComponent } from "./form-search/complementary-filters/label/label.component";
+import { PricesComponent } from "./form-search/complementary-filters/prices/prices.component";
+import { StopsComponent } from "./form-search/complementary-filters/stops/stops.component";
+import { FormSearchComponent } from "./form-search/form-search.component";
+import { HeaderComponent } from "./header/header.component";
+import { ModalComponent } from "./modal/modal.component";
+import { TicketComponent } from "./ticket/ticket.component";
+import { MaterialModule } from "../core/material/material.module";
+import { ReactiveFormsModule } from "@angular/forms";
+
+@NgModule({
+  declarations: [
+    HeaderComponent,
+    BannerComponent,
+    ContainerComponent,
+    FooterComponent,
+    CardComponent,
+    CardSearchComponent,
+    CardTestimonyComponent,
+    FormSearchComponent,
+    ModalComponent,
+    CounterComponent,
+    DropdownUfComponent,
+    FormBaseComponent,
+    SearchComponent,
+    TicketComponent,
+    AirlinesComponent,
+    LabelComponent,
+    ComplementaryFiltersComponent,
+    PricesComponent,
+    StopsComponent,
+  ],
+  imports: [
+    CommonModule,
+    MaterialModule,
+    ReactiveFormsModule
+  ],
+  exports: [
+    HeaderComponent,
+    BannerComponent,
+    ContainerComponent,
+    FooterComponent,
+    CardComponent,
+    CardSearchComponent,
+    CardTestimonyComponent,
+    FormSearchComponent,
+    ModalComponent,
+    CounterComponent,
+    DropdownUfComponent,
+    FormBaseComponent,
+    SearchComponent,
+    TicketComponent,
+    AirlinesComponent,
+    LabelComponent,
+    ComplementaryFiltersComponent,
+    PricesComponent,
+    StopsComponent
+  ]
+})
+export class SharedModule {}
